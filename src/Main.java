0a5ed3f922724f139b9d6a61b312efc186538dbe
OmidHdr <omidnet42@yahoo.com>
2022-08-14 22:17:05 +0430

    add switch element

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         BigInteger mult;
+        String str = null;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your First Big number : ");
         BigInteger number1 = input.nextBigInteger();
         System.out.println("Enter your secound Big number : ");
         BigInteger number2 = input.nextBigInteger();
-        mult = number1.multiply(number2);
-        System.out.println(mult);
+        input.nextLine();
+        System.out.println("Enter your Operator(+ - / * ) : ");
+        str = input.nextLine();
+        switch (str){
+            case("*"):
+                mult = number1.multiply(number2);
+                System.out.println(mult);
+                break;
+            case("+"):
+                mult = number1.add(number2);
+                System.out.println(mult);
+                break;
+            case("-"):
+                mult = number1.subtract(number2);
+                System.out.println(mult);
+                break;
+            case("/"):
+                mult = number1.divide(number2);
+                System.out.println(mult);
+                break;
+        }
+
+
+
     }
 }

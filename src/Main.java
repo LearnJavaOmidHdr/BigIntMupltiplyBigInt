e19243f7a6eaefeb6c14d87876ac12fe5537c8dd
OmidHdr <omidnet42@yahoo.com>
2022-08-15 10:30:16 +0430

    Tring make own class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,65 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    /*
+    public static void multi() {
+        String[] one, two, nn;
+        int ba = 0, len1, len2;
+        int firstNumber, secondNumber;
+        int f = 0, s = 0, r = 0;
+//        String[] result ;
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        System.out.println("Enter your First Big number : ");
+        String number1 = input.nextLine();
+        System.out.println("Enter your secound Big number : ");
+        String number2 = input.nextLine();
+        one = number1.split("");
+        two = number2.split("");
+        for (String s1 : one) {
+            int x = Integer.parseInt(s1);
+            list1.add(x);
+        }
+        for (String s2 : two) {
+            int x = Integer.parseInt(s2);
+            list2.add(x);
+        }
+        len1 = list1.size();
+        len2 = list2.size();
+        for (int i = len1; i >= 0; i--) {
+            f = list1.get(i - 1);
+            s = list2.get(i - 1);
+            f *= s;
+            f += ba;
+            String n = "";
+            n = String.valueOf(f);
+            nn = n.split("");
+            if (nn.length > 1) {
+//                result.add(Integer.parseInt(nn[1]));
+                String rr ="";
+                rr = nn[1];
+                System.out.print(rr);
+                ba = Integer.parseInt(nn[0]);
+            }else{
+//                result.add(Integer.parseInt(nn[0]));
+                String rr = "";
+                rr = nn[0];
+                System.out.print(rr);
+                ba = 0;
+            }
+            System.out.println(result);
+        }
+    }
+    */
+
+
+
+
+
     public static void main(String[] args) {
         BigInteger mult;
         String str = null;
@@ -31,8 +89,7 @@ public class Main {
                 System.out.println(mult);
                 break;
         }
-
-
+//        multi();
 
     }
 }
